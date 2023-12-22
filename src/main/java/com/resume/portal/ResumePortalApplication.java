ca778044fa0ab3ef2a7cad0ddb20b98cbdbb4c5a
Ashutosh <ashutosh211gupta@gmail.com>
2023-12-22 20:16:09 +0530

    Initial commit

---
+++ src/main/java/com/resume/portal/ResumePortalApplication.java
@@ -0,0 +1,13 @@
+package com.resume.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumePortalApplication.class, args);
+	}
+
+}

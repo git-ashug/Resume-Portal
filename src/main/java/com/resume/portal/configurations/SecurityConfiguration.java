021f4228649a42470f5c1c4be7a7077d360e79d4
Ashutosh <ashutosh211gupta@gmail.com>
2023-12-30 15:45:53 +0530

    Added resume templates and some minor modifications

--- src/main/java/com/resume/portal/configurations/SecurityConfiguration.java
+++ src/main/java/com/resume/portal/configurations/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,10 +41,11 @@ public class SecurityConfiguration {
 				.authorizeHttpRequests(auth -> {
 					auth.requestMatchers("/edit").authenticated();
 					auth.requestMatchers("/h2-console/**").permitAll();
-					auth.requestMatchers("/*").permitAll();
+					auth.requestMatchers("/**").permitAll();
 				})
 				.headers(headers -> headers.frameOptions().sameOrigin())
-				.formLogin().and().build();
+				.formLogin().and()		//or can use  .httpBasic(Customizer.withDefaults())
+				.build();
 	}
 	
 }
--- src/main/java/com/resume/portal/controller/HomeController.java
+++ src/main/java/com/resume/portal/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.resume.portal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping("/")
@@ -14,5 +18,22 @@ public class HomeController {
 	@GetMapping("/edit")
 	public String edit() {
 		return "Edit Page";
+	}
+	
+	@GetMapping("/test1")
+	public String test1() {
+		return "resume-templates/1/index";		//no need to append .html
+	}
+	
+	@GetMapping("/test2")
+	public String test2() {
+		return "resume-templates/2/index.html";
+	}
+	
+	
+	@GetMapping("/view/{userName}")
+	public String view(@PathVariable String userName, Model model) {
+		model.addAttribute("userName",userName);
+		return "profile";
 	}
 }

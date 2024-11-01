56b2d5cb20ff2db847263f37d94390976c313558
Ashutosh <ashutosh211gupta@gmail.com>
2024-11-01 20:17:30 +0530

    Added exception handling mechanism

---
+++ src/main/java/com/resume/portal/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.resume.portal.exceptions;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+}
